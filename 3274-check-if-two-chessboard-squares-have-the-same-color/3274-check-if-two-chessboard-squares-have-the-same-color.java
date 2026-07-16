156bcf1241f3a499ee8962bf4201a117ff3dd566
Mohit Kumar Gupta <itzzmohitguptaa@gmail.com>
2026-07-16 12:12:37 +0530

    Time: 0 ms (100%), Space: 43.2 MB (85.15%) - LeetHub

--- 3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.java
+++ 3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.java
@@ -1,28 +1,8 @@
 class Solution {
     public boolean checkTwoChessboards(String coordinate1, String coordinate2) {
-        if(isBlackorWhite(coordinate1.charAt(0),coordinate1.charAt(1)).equals("black") && isBlackorWhite(coordinate2.charAt(0),coordinate2.charAt(1)).equals("black")){
+      int targetSum1 = (coordinate1.charAt(0) - 'a' + 1) + (coordinate1.charAt(1) - '0');
+      int targetSum2 = (coordinate2.charAt(0) - 'a' + 1) + (coordinate2.charAt(1) - '0');
 
-            return true;
-        }
-        else if(isBlackorWhite(coordinate1.charAt(0),coordinate1.charAt(1)).equals("white") && isBlackorWhite(coordinate2.charAt(0),coordinate2.charAt(1)).equals("white")){
-            return true;
-        }
-        return false;
-    }
-
-    public String isBlackorWhite(char ch, char num){
-        if(ch == 'a' || ch == 'c' || ch == 'e' || ch == 'g'){
-            if(num == '1' || num == '3' || num == '5' || num == '7'){
-                return "black";
-            }
-        }
-
-          if(ch == 'b' || ch == 'd' || ch == 'f' || ch == 'h'){
-            if(num == '2' || num == '4' || num == '6' || num == '8'){
-                return "black";
-            }
-        }
-        return "white";
-
+    return targetSum1 % 2 == targetSum2 % 2;
     }
 }
